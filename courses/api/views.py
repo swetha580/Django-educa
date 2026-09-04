699f0841cd553732933626e34fe2f9a5981e5e06
Swetha <swetha@testpress.in>
2026-09-04 15:48:44 +0530

    Add custom IsEnrolled permission and course contents API endpoint

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, viewsets
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -18,7 +19,6 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
 
-
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
@@ -28,3 +28,8 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+
+    @action(detail=True, methods=['get'], serializer_class=CourseWithContentsSerializer,
+            authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated, IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
