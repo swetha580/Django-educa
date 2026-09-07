33a0958889679ad7e39bd71eaf12932592337722
Swetha <swetha@testpress.in>
2026-09-07 17:56:07 +0530

    Add Daphne for production WebSocket support

--- educa/asgi.py
+++ educa/asgi.py
@@ -14,3 +14,10 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings')
 
 application = get_asgi_application()
+import os
+import django
+from channels.routing import get_default_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'educa.settings.pro')
+django.setup()
+application = get_default_application()
