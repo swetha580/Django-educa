bb69a6898c75a75d7b2bd9cfaae292e37821773a
Swetha <swetha@testpress.in>
2026-09-07 15:12:46 +0530

    Split settings into base/local/pro

---
+++ educa/settings/base.py
@@ -0,0 +1,2 @@
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
---
+++ educa/settings/pro.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('swetha', 'swetha@testpress.in'),
+)
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {}
+}
